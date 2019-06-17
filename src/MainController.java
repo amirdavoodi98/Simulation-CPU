9d36f0195a4609a5024f35db65ca21604c97723e
mohdis <42219180+mohdis@users.noreply.github.com>
2019-06-17 13:39:17 +0430

    Update MainController

--- src/MainController.java
+++ src/MainController.java
@@ -1,35 +1,71 @@
+
 import javafx.stage.Stage;
 
 public class MainController {
-    private boolean RegDst = false, RegWrite = false, ALUSrc = false, PCSrc = false, MemRead = false,
-            MemWrite = false, MemtoReg = false, Branch = false;
-    private int registers[] = new int[32];
-    private int times_used_register[] = new int[32];
+
+    public static boolean RegDst = false, RegWrite = false, ALUSrc = false, MemRead = false,
+            MemWrite = false, MemtoReg = false, Branch = false , Jump = false, Jalr = false,Lui= false;
+
+    public static int pc = 0;
+    public  static int memory[] = new int[65536];
+    public  static String instructionCode = "";
+    public static String AlUOp = "";
+    public static int readMemory = 0;
+    public static int readReg1 = 0 ;
+    public static int readReg2 = 0 ;
+    public static int ALUResult = 0 ;
+
+    public static int registers[] = new int[32];
+
+    private static int times_used_register[] = new int[32];
     private static int mem_cells_used = 0;
     static Stage myStage;
 
     static void execute() {
+
+        setupRegisters();//setup registers
+
         Assembler.main();
         ThreadHelper threadHelper = new ThreadHelper();
         threadHelper.run();
     }
 
-    private static int getMemUsed() {
-        // TODO: Iterate over memory to find out how many cells of that have been used?
+
+    private static void checkJump(){
+
+        if(ALUResult == 0 && Branch){
+            pc += (int) Utilities.getDecimal(instructionCode.substring(16,32))+1;
+        }
+        if(Jump){
+            pc = (int) Utilities.getDecimal(instructionCode.substring(16,32));
+        }
+
+        if(Jalr){
+            pc = (int) Utilities.getDecimal(instructionCode.substring(16,32));
+        }
+    }
+
+
+    private static int getMemUsed() { // TODO: Iterate over memory to find out how many cells of that have been used?
+
         return 0;
     }
+    private static void setTimesOfUsing(int register_id) {        // TODO: calculate how many times one register has been used
 
-    private static void setRegisters(String code) {
-        // TODO: set each register's value
     }
 
-    private static void setTimesOfUsing(int register_id) {
-        // TODO: calculate how many times one register has been used
+
+
+    private static void setupRegisters(){
+
+        for(int i = 0;i<registers.length;i++){
+
+            registers[i] = 0;
+
+        }
     }
 
-    private static void setSignals(String code) {
-        // TODO: set each signal whether is false or true
-    }
+
 
     private static void changeImageViews() {
         // TODO: set each imageView whether is enable or disable
@@ -39,15 +75,17 @@ public class MainController {
         @Override
         public void run() {
 
-            for (int pc = 0; pc < 10; pc++) {
-                if (pc > Assembler.limit) {
-                    break;
-                }
-                String code = Assembler.getBinaryCode(pc);
-                System.out.println(code);
-                setSignals(code);
+            for (pc = 0; pc < Assembler.limit; pc++) {
+
+                instructionCode = Assembler.getBinaryCode(pc);
+                System.out.println(instructionCode);
+                ControlUnit.setSignals();
+                Registers.readRegisters();
+                ALU.ALUExecution();
+                checkJump();
+                Memory.memoryManager();
+                Registers.writeBack();
                 changeImageViews();
-                setRegisters(code);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -58,4 +96,3 @@ public class MainController {
         }
     }
 }
-
