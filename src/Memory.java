1141cd5c77da92a6fafaed037b3440f364a21c23
mohdis <42219180+mohdis@users.noreply.github.com>
2019-06-17 21:51:19 +0430

    get Mem Used Added

--- src/Memory.java
+++ src/Memory.java
@@ -1,13 +1,18 @@
 //package com.company;
 
 public class Memory {
-
+    
+ private static int getMemUsed() { // TODO: Iterate over memory to find out how many cells of that have been used?
+      MainController.mem_cells_used++;
+        return 0;
+    }
     public static void memoryManager(){
         if(MainController.MemRead){
+            getMemUsed();
             MainController.readMemory = MainController.memory[MainController.ALUResult];
         }
         if(MainController.MemWrite){
-
+            getMemUsed();
             MainController.memory[MainController.ALUResult] = MainController.readReg2;
 
         }
